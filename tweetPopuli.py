c941971073d99e50491005440eff318d7123e054
Connor <43657700+c-merritt3@users.noreply.github.com>
2019-09-11 17:06:30 -0400

    Add files via upload

---
+++ tweetPopuli.py
@@ -0,0 +1,72 @@
+import tweepy 
+import secret
+import re
+from textblob import TextBlob
+
+auth = tweepy.OAuthHandler(secret.consumer_key, secret.consumer_secret) 
+auth.set_access_token(secret.access_token, secret.access_secret)
+api = tweepy.API(auth, wait_on_rate_limit = True)
+
+def tweet_sentiment(tweet):
+    '''
+    using a NLP library textblob method, return if the tweet was considered neg or pos
+    '''
+    analysis = TextBlob(tweet)
+    if analysis.sentiment.polarity > .10:
+        return 'Positive'
+    else:
+        return 'Negative'
+
+def clean_tweet(tweet):  
+    '''
+    using regex expressions "clean up" the tweet
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split()) 
+
+def getReplies(username, user_tweets):
+    '''
+    get replies of a politcians tweet, take sentiment of each response
+    '''
+    responses_to_user = []
+    for tweet in user_tweets:
+        print(tweet.text)
+        replies = tweepy.Cursor(api.search, q='to:{}'.format(username),since_id= tweet.id, result_type = 'mixed', tweet_mode='extended').items()
+        while True:
+            reply = replies.next()
+            responses_to_user.append(tweet_sentiment(clean_tweet(reply.full_text)))
+            if len(responses_to_user) > 100:
+                break;
+    return responses_to_user
+
+def pos_neg_sort(replies_to_user, name):
+    '''
+    sort through replies, put them into negative or positive tweets
+    '''
+    ptweets = []
+    ntweets = []
+    for tweets in replies_to_user:
+        if tweets == "Positive":
+            ptweets.append(tweets)
+        elif tweets == "Negative":
+            ntweets.append(tweets)
+    
+    print("{} pos tweet percentage : {} %".format(name, 100*len(ptweets)/len(replies_to_user)))
+    print("{} neg tweet percentage : {} % \n".format(name, 100*len(ntweets)/len(replies_to_user)))
+    
+def get_tweets_for_user(user_screen_name):
+    '''
+    getting the actual tweet for each user
+    '''
+    screen_name_tweets = []
+    screen_name_tweets = api.user_timeline(screen_name = user_screen_name, count = 1, include_rts = False)
+    return screen_name_tweets
+
+politician_list = ["realDonaldTrump", "AOC","JoeBiden", "SenWarren","BorisJohnson","marcorubio"] 
+for politician in politician_list:
+    politician_tweets = get_tweets_for_user(politician)
+    replies_to_politcian = getReplies(politician, politician_tweets)
+    pos_neg_sort(replies_to_politcian,politician)
+
+
+
+
